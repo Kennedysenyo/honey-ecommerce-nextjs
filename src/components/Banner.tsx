bdd9a59065294819fe1011d90600c8329e07a26a
kennedysenyo <kennedysenyo@gmail.com>
2026-03-26 20:50:40 +0000

    add animation to banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { motion } from "motion/react";
+
 interface Props {
   title: string;
   subtitle: string;
@@ -15,12 +19,17 @@ export const Banner = ({ title, subtitle, image }: Props) => {
       <div className="absolute inset-0 bg-black/45 " />
       <div className="absolute inset-0 z-10 pt-12 flex flex-col justify-center  backdrop-blur-md">
         <div className="section-max-w mx-auto section-px-one">
-          <div className="p-6 rounded-2xl text-center text-background w-full space-y-2 md:space-y-4 max-w-4xl mx-auto">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className="p-6 rounded-2xl text-center text-background w-full space-y-2 md:space-y-4 max-w-4xl mx-auto"
+          >
             <h1 className="font-heading title-one text-gold text-shadow-sm">
               {title}
             </h1>
             <p className="subtitle-one">{subtitle}</p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
