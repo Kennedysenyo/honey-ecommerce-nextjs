88c88e603736ad56a26be7c541ef7e41ec79fe3e
kennedysenyo <kennedysenyo@gmail.com>
2026-03-28 17:30:48 +0000

    reduce search bar size

--- src/components/shop/SearchBar.tsx
+++ src/components/shop/SearchBar.tsx
@@ -10,19 +10,21 @@ interface Props {
 
 export const SearchBar = memo(({ value, handler }: Props) => {
   return (
-    <div className="absolute top-4 z-30 mx-auto w-full section-px-one flex ">
-      <div className="mx-auto max-w-lg  w-full relative">
-        <span>
-          <Search className="icon2 absolute left-4 top-1/2 -translate-y-1/2 text-amber/70" />{" "}
-        </span>
-        <input
-          aria-label="Search product"
-          placeholder="Search product..."
-          name="product"
-          className="bg-background pl-10 pr-6 shadow-md text-amber py-3 w-full rounded-full text-sm sm:text-base md:text-lg border-2 border-gold placeholder:text-lg placeholder:text-amber/70 focus:outline-none "
-          value={value}
-          onChange={handler}
-        />
+    <div className="absolute top-4 z-30 w-full section-px-one flex ">
+      <div className="section-max-w w-full mx-auto flex items-center justify-center sm:justify-end">
+        <div className="max-w-xs sm:max-w-sm    w-full relative">
+          <span>
+            <Search className="icon2 absolute left-4 top-1/2 -translate-y-1/2 text-amber/70" />
+          </span>
+          <input
+            aria-label="Search product"
+            placeholder="Search product..."
+            name="product"
+            className="bg-background pl-10 pr-6 shadow-md text-amber py-2 w-full rounded-full text-sm sm:text-base border-2 border-gold placeholder:text-base placeholder:text-amber/70 focus:outline-none "
+            value={value}
+            onChange={handler}
+          />
+        </div>
       </div>
     </div>
   );
